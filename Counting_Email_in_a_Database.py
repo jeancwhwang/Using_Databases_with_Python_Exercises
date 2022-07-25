cff700d9c1189ff87bc1ecc85ac375ae566e50c1
Jean Hwang <106926262+jeancwhwang@users.noreply.github.com>
2022-07-25 15:00:37 -0500

    Update Counting_Email_in_a_Database.py

--- Counting_Email_in_a_Database.py
+++ Counting_Email_in_a_Database.py
@@ -6,34 +6,34 @@
 
 import sqlite3
 
-conn = sqlite3.connect('orgs.sqlite')
-cur = conn.cursor()
+conn = sqlite3.connect('orgdb.sqlite') # connect a file & create a dbfile
+cur = conn.cursor() 
 
 cur.execute('DROP TABLE IF EXISTS Counts')
 
-cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
+cur.execute('''CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
-fname = input('Enter file name: ')
-if len(fname) < 1:
-    fname = 'mbox.txt'
+fname = input('Enter file name:')
+if (len(fname) < 1): fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
-    if not line.startswith('From: '):
-        continue
+    if not line.startswith('From:'): continue
     pieces = line.split()
     email = pieces[1]
     org = email.split('@')[1]
-    cur.execute('SELECT count FROM Counts WHERE org = ?', (org, ))
-    try:
-        count = cur.fetchone()[0]
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?', (org, ))
-    except:
-        cur.execute('INSERT INTO Counts (org, count) VALUES (?, 1)', (org, ))
+    cur.execute('SELECT count FROM Counts WHERE org = ?', (org,)) #? 
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+        VALUES (?, 1) ''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?', (org,))
 
-conn.commit()
+    conn.commit()
 
+#http://www.sqlite.org/lang_select.html
 sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
-for row in conn.execute(sqlstr):
-    print(row[0], row[1])
 
-conn.close()
+for row in conn.execute(sqlstr):
+    print(str(row[0]), row[1])
+cur.close()
